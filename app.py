247f21f2f77374b463a7181baa05bc7eef5c1aa4
asutosh <asutosh407@gmail.com>
2018-07-18 21:30:35 +0530

    Day 4

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask 
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash,check_password_hash
+
 app=Flask(__name__)
 
 app.config['SECRET_KEY']='thisverysecreatdontsharewithanyone'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////c/Work/sqldb/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///todo.db'
 
 db= SQLAlchemy(app)
 
@@ -17,6 +20,41 @@ class User(db.Model):
 class Todo(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     text=db.Column(db.String(50))
+
+
+@app.route('/user',methods=['GET'])
+def get_all_user():
+    users=User.query.all()
+    output=[]
+    for user in users:
+        user_data={}
+        user_data['public_id']=user.public_id
+        user_data['name']=user.name
+        user_data['password']=user.password
+        user_data['admin']=user.admin
+        output.append(user_data)
+    return jsonify({"users":output})
+
+@app.route('/user/<user_id>',methods=['GET'])
+def get_one_user():
+    return ''
+
+@app.route('/user',methods=['POST'])
+def create_user():
+    data=request.get_json()
+    hashed_password=generate_password_hash(data['password'],method='sha256')
+    new_user=User(public_id=str(uuid.uuid4()),name=data['name'],password=hashed_password,admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message':'new user created'})
+
+@app.route('/user/<user_id>',methods=['PUT'])
+def promte_user():
+    return ''
+
+@app.route('/user/<user_id>',methods=['PUT'])
+def delete_user():
+    return ''
 
 
 
